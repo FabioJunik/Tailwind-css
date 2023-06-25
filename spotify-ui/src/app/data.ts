d08da1e5c69dc6314c0e40d524a39fc601e39f17
fabiojunik <fabiojunik@gmail.com>
2023-06-25 20:26:06 +0100

    feat: create fake data

---
+++ spotify-ui/src/app/data.ts
@@ -0,0 +1,61 @@
+export interface MusicProps {
+    id: number;
+    title: string;
+    autor: string;
+    image: string;
+}
+interface DataProps {
+    libary: string[];
+    music: MusicProps[],
+    recentlyPlayed: number[]
+}
+
+export const data: DataProps = {
+    libary: [
+        "Minhas Músicas",
+        "louvores",
+        "NerdHit",
+        "Em inglês 1",
+        "Para relaxar",
+        "Só mais uma"
+    ],
+    music: [
+        {
+            id: 1,
+            title: "Me atraiu",
+            autor: "Gabriela Rocha",
+            image: "/gabriela.jpg"
+        },
+        {
+            id: 2,
+            title: "Tudo Coopera Para o Meu Bem",
+            autor: "Ministério Zoe",
+            image: "/zoecopera.jpg"
+        },
+        {
+            id: 3,
+            title: "Esperança",
+            autor: "Julia Vitoria e Gabriel Guedes",
+            image: "/juliagabrielesperanca.jpg"
+        },
+        {
+            id: 4,
+            title: "Os Sonhos de Deus",
+            autor: "Preto no Branco",
+            image: "/pretobranco.jpeg"
+        },
+        {
+            id: 5,
+            title: "O Verbo",
+            autor: "Clovis",
+            image: "/gabriela.jpg"
+        },
+        {
+            id: 6,
+            title: "Deserto",
+            autor: "Maria Marçal",
+            image: "/gabriela.jpg"
+        }
+    ],
+    recentlyPlayed: [3,5,1,2]
+}
